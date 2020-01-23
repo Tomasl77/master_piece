cd0e6141155516c7bb7886e3bf87ed34198ee82c
Tomas Lobgeois <lobgeois.tomas@free.fr>
2020-01-23 16:12:13 +0100

    api

---
+++ masterpiece-api/src/test/java/fr/formation/masterpiece/MasterpieceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.formation.masterpiece;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterpieceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
