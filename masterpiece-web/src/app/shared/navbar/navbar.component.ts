bfd91052b65ee3ea02dcf9e02e72a170507c49cb
Tomas Lobgeois <lobgeois.tomas@free.fr>
2020-01-24 17:29:08 +0100

    Angular navbar in progress

---
+++ masterpiece-web/src/app/shared/navbar/navbar.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, ViewEncapsulation, AfterViewInit, Output, EventEmitter, HostListener } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class NavbarComponent implements OnInit {
+
+
+  constructor(
+  
+  ) {
+
+  }
+
+  ngOnInit() {
+  }
+ 
+  
+}
---
+++ masterpiece-web/src/app/shared/shared.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MatMenuModule} from '@angular/material/menu';
+
+@NgModule({
+  declarations: [NavbarComponent],
+  imports: [
+    CommonModule,
+    MatMenuModule
+  ],
+  exports: [
+    NavbarComponent
+  ]
+})
+export class SharedModule { }
