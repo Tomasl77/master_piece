251855a2aadae599c0cbf6ade0a977e2ea98db83
Tomasl77 <lobgeois.tomas@free.fr>
2020-09-08 18:35:14 +0200

    member id is now the requester (was the user before)

--- masterpiece-api/src/main/java/fr/formation/masterpiece/repositories/MemberRepository.java
+++ masterpiece-api/src/main/java/fr/formation/masterpiece/repositories/MemberRepository.java
@@ -3,6 +3,7 @@ package fr.formation.masterpiece.repositories;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import fr.formation.masterpiece.domain.dtos.views.MemberInfoViewDto;
 import fr.formation.masterpiece.domain.entities.Member;
@@ -10,4 +11,7 @@ import fr.formation.masterpiece.domain.entities.Member;
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     Optional<MemberInfoViewDto> getById(Long id);
+
+    @Query("select id from Member m where m.user.id = :id")
+    Long getMemberIdByUserId(Long id);
 }
--- masterpiece-api/src/main/java/fr/formation/masterpiece/services/impl/SubjectManagerServiceImpl.java
+++ masterpiece-api/src/main/java/fr/formation/masterpiece/services/impl/SubjectManagerServiceImpl.java
@@ -6,9 +6,9 @@ import org.springframework.stereotype.Service;
 
 import fr.formation.masterpiece.config.security.SecurityHelper;
 import fr.formation.masterpiece.domain.dtos.SubjectDto;
-import fr.formation.masterpiece.domain.entities.UserAuth;
+import fr.formation.masterpiece.domain.entities.Member;
 import fr.formation.masterpiece.domain.entities.Subject;
-import fr.formation.masterpiece.repositories.UserJpaRepository;
+import fr.formation.masterpiece.repositories.MemberRepository;
 import fr.formation.masterpiece.repositories.SubjectRepository;
 import fr.formation.masterpiece.services.SubjectManagerService;
 
@@ -17,27 +17,28 @@ public class SubjectManagerServiceImpl implements SubjectManagerService {
 
     private final SubjectRepository repository;
 
-    private final UserJpaRepository userRepository;
+    private final MemberRepository memberRepository;
 
     @Autowired
     private ModelMapper mapper;
 
     public SubjectManagerServiceImpl(SubjectRepository repository,
-            UserJpaRepository userRepository) {
+            MemberRepository memberRepository) {
 	this.repository = repository;
-	this.userRepository = userRepository;
+	this.memberRepository = memberRepository;
     }
 
     @Override
     public void create(SubjectDto dto) {
-	Long id = SecurityHelper.getUserId();
+	Long userId = SecurityHelper.getUserId();
+	Long memberId = memberRepository.getMemberIdByUserId(userId);
 	Subject subject = new Subject();
 	subject.setCategory(dto.getCategory());
 	subject.setDescription(dto.getDescription());
 	subject.setTitle(dto.getTitle());
 	subject.setVote(0);
-	UserAuth user = userRepository.getOne(id);
-	subject.setUser(user);
+	Member member = memberRepository.getOne(memberId);
+	subject.setMember(member);
 	repository.save(subject);
     }
 
