914ea556e35875abd79ff45833d03de53ab29e57
Tomasl77 <lobgeois.tomas@free.fr>
2020-12-21 19:04:59 +0100

    remove debut for mail

--- masterpiece-server/src/main/java/fr/formation/masterpiece/commons/config/BeanConfig.java
+++ masterpiece-server/src/main/java/fr/formation/masterpiece/commons/config/BeanConfig.java
@@ -101,7 +101,6 @@ public class BeanConfig {
 	props.put("mail.smtp.auth", "true");
 	props.put("mail.smtp.starttls", "true");
 	props.put("mail.smtp.starttls.enable", "true");
-	props.put("mail.debug", "true");
 	return javaMailSender;
     }
 
